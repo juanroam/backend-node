0b997cb75f8d8b3f2f8dc2828397240577aa33d7
juanroam <juanc.rodrigueza@hotmail.com>
2020-01-19 20:52:00 -0500

    Integración de frontend, con proyecto Vue.js
    Integración de proyecto frontend minificado en la carpeta public de
    node.
    Instalación de modulo CORS (Cross-origin resource sharing), para
    determinar de donde vienen las cabeceras.

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const socket = require('./socket')
 const db = require('./db')
 const router = require('./network/routes')
 
 db('mongodb+srv://db_user_roam:4q9772t3bThunZW@cluster0-myaoh.mongodb.net/test')
+
+app.use(cors())
 
 const hostname = "127.0.0.1"
 const port = 3000
@@ -20,7 +23,7 @@ socket.connect(server)
 // pasa el servidor express al router para crear todas las rutas necesarias
 router(app)
 
-app.use('/app', express.static('public'))
+app.use('/', express.static('public'))
 
 server.listen(port, function() {
     console.log(`Server running at http://${hostname}:${port}/`)
